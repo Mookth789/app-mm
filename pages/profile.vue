123d470085709875acd8a28e79553b167135b173
Mookth789 <135083436+Mookth789@users.noreply.github.com>
2023-08-24 23:07:01 +0700

    2 files added, 2 deleted

---
+++ pages/profile.vue
@@ -0,0 +1,21 @@
+<template>
+<div id="app">
+			<div class="container container22243">
+				<div class="diygw-title diygw-top solid-bottom" style="color: white; background: #ff32ad">
+					<div class="title flex1 text-center">ข้อมูลส่วนตัว</div>
+				</div>
+      </div>
+
+
+</div>
+</template>
+
+
+<script setup>
+
+</script>
+
+
+<style>
+
+</style>
